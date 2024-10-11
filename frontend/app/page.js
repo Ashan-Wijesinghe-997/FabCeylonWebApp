1590002e08df85563e755a16808a19a20ee86e15
Ashan-Wijesinghe-997 <ashanbuddhika997@gmail.com>
2024-10-11 17:22:10 +0530

    initial commit

---
+++ frontend/app/page.js
@@ -0,0 +1,53 @@
+import { FloatingNavDemo } from "@/components/Floatingnav";
+
+
+
+
+export default function Home() {
+  return (
+    
+    <div>
+      <FloatingNavDemo />
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+      <p>halkahdfkahkfojaodfjadf<br/></p>
+    </div>
+  );
+}
